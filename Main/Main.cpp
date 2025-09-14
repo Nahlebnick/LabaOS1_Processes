dadc3e1704e37aa329b922657547c22505696ff0
Nahlebnick <kotstepa2021@gmail.com>
2025-09-14 15:01:11 +0300

    First commit

---
+++ Main/Main.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+#include <conio.h>
+#include "employee.h"
+#pragma warning (disable:4996)
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    string binfile;
+    int count;
+    cout << "Введите имя бинарного файла: ";
+    cin >> binfile;
+    cout << "Введите количество записей: ";
+    cin >> count;
+    binfile += " ";
+
+    char lpszCommandLine[] = "Creator.exe ";
+    strcat(lpszCommandLine, binfile.c_str());
+    strcat(lpszCommandLine, to_string(count).c_str());
+    STARTUPINFO si = { sizeof(si) };    
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    si.cb = sizeof(STARTUPINFO);
+
+    if (!CreateProcess(NULL, NULL,
+        NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+    {
+        cout << "Error: cannot start Creator" << endl;
+        _getch();
+        return 0;
+    }
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    string reportfile;
+    double rate;
+    cout << "\nВведите имя файла отчета: ";
+    cin >> reportfile;
+    cout << "Введите оплату за час: ";
+    cin >> rate;
+
+    // Запуск Reporter
+    string cmdReporter = "Reporter.exe " + binfile + " " + reportfile + " " + to_string(rate);
+    if (!CreateProcess(NULL, NULL,
+        NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+    {
+        cout << "Error: cannot start Reporter" << endl;
+        _getch();
+        return 0;
+    }
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    return 0;
+}
---
+++ Reporter/Reporter.cpp
@@ -0,0 +1,51 @@
+﻿// Reporter.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "employee.h"
+#pragma warning (disable:4996)
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    if (argc < 4)
+    {
+        std::cout << "Usage: Reporter <binfile> <reportfile> <hourly_wage>" << std::endl;
+        return 0;
+    }
+
+    FILE* f;
+    f = fopen(argv[1], "rb");
+
+    if (!f)
+    {
+        std::cout << "Ошибка открытия файла!" << std::endl;
+        return 0;
+    }
+
+    std::string reportFileName = argv[2];
+
+    double hourlyWage = std::stod(argv[3]);
+
+    freopen(reportFileName.c_str(), "w", stdout);
+
+    cout << "Отчет по файлу \"" << argv[1] << "\"" << endl;
+    cout << "Номер\tИмя\tЧасы\tЗарплата" << endl;
+
+    employee emp;
+    while (fread(&emp, sizeof(struct employee), 1, f))
+    {
+        double salary = hourlyWage * emp.hours;
+        cout << fixed << setprecision(2);
+        cout << emp.num << "\t" << emp.name << "\t" << emp.hours << "\t" << salary << endl;
+    }
+    fclose(f);
+
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
